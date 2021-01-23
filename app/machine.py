1c993f3240a724819142ab7a85dea3e3065233c8
David Axelrod <daxaxelrod@gmail.com>
2021-01-23 14:09:04 -0500

    including the timeout enabled machine

--- app/machine.py
+++ app/machine.py
@@ -3,25 +3,25 @@ from app.constants.fsm import transitions, states
 from .fsm import DrinkManufacturerFSM
 from transitions import Machine
 from app.models import Drink
+from transitions.extensions.states import add_state_features, Timeout
+
+
+@add_state_features(Timeout)
+class TimeoutEnabledMachine(Machine):
+    pass
 
 
 class DrinkManufacturer(object):
 
     def __init__(self):
         self.model = DrinkManufacturerFSM()
-        self.machine = Machine(model=self.model, states=states, transitions=transitions, initial='idle')
+        self.machine = TimeoutEnabledMachine(model=self.model, states=states, transitions=transitions, initial='idle')
 
     def awaken(self):
         self.machine.wake()
 
         # if after 2 minutes no selection has been made, revert back to idle
-        now = time.time() 
-        timeout = now + 120 # seconds
-        while now < timeout:
-            time.sleep(1)
-        if self.machine.state == "selecting":
-            self.machine.sleep()
-            
+        
 
     def on_drink_selection(self, drink: Drink):
         self.machine.prepare_drink(drink)
