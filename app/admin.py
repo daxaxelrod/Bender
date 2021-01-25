60c58d2411a6e8524f2f8e7532785ced697d1383
David Axelrod <daxaxelrod@gmail.com>
2021-01-25 02:59:31 -0500

    admin motor power works

--- app/admin.py
+++ app/admin.py
@@ -31,24 +31,26 @@ class ResevoirAdmin(admin.ModelAdmin):
         return obj.a + obj.b
 
 MOTOR_MAP = {
-    6: machine.model.horizontal_patter_motor
-    17: machine.model.shaker_motor
-    27: machine.model.vertical_platter_motor
+    6: machine.machine.model.horizontal_patter_motor,
+    17: machine.machine.model.shaker_motor,
+    27: machine.machine.model.vertical_platter_motor
 }
 
-class AxisMotorAdmin(admins.ModelAdmin):
+class AxisMotorAdmin(admin.ModelAdmin):
 
-    def drive_motors_in_direction(direction, queryset):
-        machine.model.set_global_direction(direction)
+    def drive_motors_in_direction(self, direction, queryset):
+        machine.machine.model.set_global_direction(direction)
         ids = [x.gpio_pin for x in queryset]
         for id in ids:
             MOTOR_MAP[id].drive()
 
     def drive_motor_forward(modeladmin, request, queryset):
-        self.drive_motors_in_direction(True, queryset)
-        
+        modeladmin.drive_motors_in_direction(True, queryset)
+
     def drive_motor_backwards(modeladmin, request, queryset):
-        self.drive_motors_in_direction(False, queryset)
+        modeladmin.drive_motors_in_direction(False, queryset)
+
+    actions = [drive_motor_forward, drive_motor_backwards]
 
 # Register your models here.
 admin.site.register(Ingredient)
