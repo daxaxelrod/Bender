adce0fc9600bdf0ea9d3a288a4d8596aaf4b529c
David Axelrod <daxaxelrod@gmail.com>
2021-01-25 01:58:38 -0500

    Fixed cross origin issue, originated with my fetch call

--- app/templates/bender-ui/src/components/Selection.js
+++ app/templates/bender-ui/src/components/Selection.js
@@ -34,7 +34,6 @@ export default function Selection() {
             headers: {
                 Accept: "application/json",
                 'Content-Type': "application/json",
-                'Access-Control-Allow-Origin': '*',//"http://localhost:8000",
             },
             body: body,
         }).then(function (response) {
