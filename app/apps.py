0ab0368e9e94a00e0166413693732bd75bc5fc05
David Axelrod <daxaxelrod@gmail.com>
2021-02-04 20:23:26 -0500

    moving model import into ready

--- app/apps.py
+++ app/apps.py
@@ -1,15 +1,15 @@
 from django.apps import AppConfig
 import RPi.GPIO as GPIO
-from app.models import Resevoir
 
 class Conf(AppConfig):
     name = 'app'
 
     def ready(self):
+        from app.models import Resevoir
         # set all resevoirs to low to trigger relays
         # high is relay open aka pump off
         GPIO.setmode(GPIO.BCM)
         resevoirs = Resevoir.objects.all()
         for res in resevoirs:
             GPIO.setup(res.gpio_pin, GPIO.OUT)
-            GPIO.output(res.gpio_pin, GPIO.LOW)
+            GPIO.output(res.gpio_pin, GPIO.HIGH)
