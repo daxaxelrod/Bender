443c1f446bd7e4b8c18b7cf8fc54e960a24d9b96
captgilligan <66520516+captgilligan@users.noreply.github.com>
2021-02-06 18:10:26 -0500

    quick script to test limit switches

---
+++ utils/limit_switch_checker.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import argparse
+import time
+
+
+parser = argparse.ArgumentParse()
+parser.add_argument("pin")
+args = parser.parse_args()
+
+pin = int(args.pin)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN)
+try:
+	while True:
+		print(GPIO.input(pin))
+		time.sleep(.1)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
